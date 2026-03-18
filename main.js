0f1c98e861f65d2a6822f6c126792da3b9fb590c
Filip Tadic <fike300705@gmail.com>
2026-03-18 13:54:11 +0100

    Finish Caesar Cipher function

--- main.js
+++ main.js
@@ -31,6 +31,35 @@ export const calculator = {
     }
 }
 
-function caeserCipher(string, shiftFactor) {
+export function caeserCipher(string, shiftFactor) {
+    const cipherArr = [];
 
+    for(let i = 0; i < string.length; i++) {
+        let charCode = string.charCodeAt(i);
+        let cipherCode = charCode + shiftFactor;
+        
+        if(Checks.upperCase(charCode)) {
+            cipherCode = cipherCode > 90 ? 64 + (cipherCode - 90) : cipherCode;
+        } else {
+            cipherCode = cipherCode > 122 ? 96 + (cipherCode - 122) : cipherCode;
+        }
+
+        if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+            cipherArr.push(String.fromCharCode(cipherCode));
+        } else {
+            cipherArr.push(String.fromCharCode(charCode));
+        }
+    }
+    return cipherArr.join('');
+}
+
+const Checks = {
+    upperCase: (charCode) => {
+        if(charCode >= 65 && charCode <= 90) return true;
+        return false;
+    },
+    lowerCase: (charCode) => {
+        if(charCode >= 97 && charCode <= 122) return true;
+        return false;
+    }
 }
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from "./main";
+import { capitalize, reverseString, calculator, caeserCipher } from "./main";
 
 it('Capitalize', () => {
     expect(capitalize('dog')).toBe('Dog');
@@ -26,4 +26,32 @@ it('Calculator (Division by Zero Check)', () => {
 
 it('Calculator (Multiply Operation)', () => {
     expect(calculator.multiply(2, 5)).toBe(10);
+})
+
+it('Caeser Cipher (1 char)', () => {
+    expect(caeserCipher('a', 1)).toBe('b');
+})
+
+it('Caeser Cipher (2 char)', () => {
+    expect(caeserCipher('ab', 1)).toBe('bc');
+})
+
+it('Caeser Cipher (3 char + shiftFactor)', () => {
+    expect(caeserCipher('abc', 2)).toBe('cde');
+})
+
+it('Caeser Cipher (Wrapping)', () => {
+    expect(caeserCipher('xyz', 3)).toBe('abc');
+})
+
+it('Caesar Cipher (Wrapping Uppercase)', () => {
+    expect(caeserCipher('XYZ', 9)).toBe('GHI');
+})
+
+it('Caesar Cipher (Preservation)', () => {
+    expect(caeserCipher('HeLLo', 3)).toBe('KhOOr');
+})
+
+it('Caesar Cipher (Punctation)', () => {
+    expect(caeserCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 })
